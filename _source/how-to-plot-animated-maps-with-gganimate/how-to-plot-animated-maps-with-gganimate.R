14ccb5610341287f5524b9362f35545f8b5f7a82
Daniela Vazquez <daniela.vazquez@gmail.com>
2017-04-25 20:32:30 -0300

    How to plot animated maps with gganimate creation (it won’t be a post, I’ll delete it) and maps in ggplotly are huge creation (will change the name later!)

---
+++ _source/how-to-plot-animated-maps-with-gganimate/how-to-plot-animated-maps-with-gganimate.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/how-to-plot-animated-maps-with-gganimate/2017-04-24-how-to-plot-animated-maps-with-gganimate.Rmd
+
---
+++ _source/maps-in-plotly-ggplotly-s-huge-maps-or-plot-geo/maps-in-plotly-ggplotly-s-huge-maps-or-plot-geo.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/maps-in-plotly-ggplotly-s-huge-maps-or-plot-geo/2017-04-24-maps-in-plotly-ggplotly-s-huge-maps-or-plot-geo.Rmd
+
