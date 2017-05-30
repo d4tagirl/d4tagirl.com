7ae57fab8bda54871182e0718f5d3bf41025ba0a
Daniela Vazquez <daniela.vazquez@gmail.com>
2017-05-30 03:06:01 -0300

    ready to merge and go live!

---
+++ _source/how-do-you-feel-about-last-week-tonight/how-do-you-feel-about-last-week-tonight.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/how-do-you-feel-about-last-week-tonight/2017-05-29-how-do-you-feel-about-last-week-tonight.Rmd
+
--- _source/john-oliver/john-oliver.R
+++
@@ -1,3 +0,0 @@
-# This R file accomanies the .Rmd blog post
-# _source/john-oliver/2017-05-26-john-oliver.Rmd
-
