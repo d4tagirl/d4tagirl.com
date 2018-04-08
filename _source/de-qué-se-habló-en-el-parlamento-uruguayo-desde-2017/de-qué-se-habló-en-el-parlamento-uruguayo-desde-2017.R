a36ba0ab3b214b208a0599aa076ea056e631f59e
Daniela Vazquez <daniela.vazquez@gmail.com>
2018-04-08 18:17:47 -0300

    new post: Tidytext analysis on uruguayan parliamentary sesions. Fixed typos in scraping post.

---
+++ _source/de-qué-se-habló-en-el-parlamento-uruguayo-desde-2017/de-qué-se-habló-en-el-parlamento-uruguayo-desde-2017.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/de-qué-se-habló-en-el-parlamento-uruguayo-desde-2017/2018-04-08-de-qué-se-habló-en-el-parlamento-uruguayo-desde-2017.Rmd
+
---
+++ _source/tf-idf-de-todas-las-sesiones-parlamentarias-analizadas/tf-idf-de-todas-las-sesiones-parlamentarias-analizadas.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/tf-idf-de-todas-las-sesiones-parlamentarias-analizadas/2018-04-08-tf-idf-de-todas-las-sesiones-parlamentarias-analizadas.Rmd
+
